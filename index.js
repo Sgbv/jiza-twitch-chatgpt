8e5b7ae20c15c81097bf86d345a4a9f19deca72f
Serge <118019874+Sgbv@users.noreply.github.com>
2023-04-12 20:10:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.get('/gpt/:text', async (req, res) => {
         model: "text-davinci-003",
         prompt: prompt,
         temperature: 0.5,
-        max_tokens: 600,
+        max_tokens: 550,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
